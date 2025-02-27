512a95c44db3c7fc53258c00ac9f69cfbaee3d5e
38099 <vladyslav.v.pats@ukd.edu.ua>
2025-02-27 21:36:31 +0200

    3-4PR

---
+++ Task1.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print(i, end=" ")
+print()
+
+for i in range(2, 21, 2):
+    print(i, end=" ")
+print()
+
+for i in range(10, 0, -1):
+    print(i, end=" ")
+print()
---
+++ Task2.py
@@ -0,0 +1,4 @@
+num = int(input("Введіть число: "))
+print(f"Таблиця множення для {num}:")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
